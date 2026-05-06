33b5dc7b9ed94d80329cf13848897d41b53ecf13
Moory <mortezafps646@gmail.com>
2026-05-06 19:03:00 +0330

    created first model
    .

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
-# Create your models here.
+class Post(models.Model):
+    # img
+    # author
+    title = models.CharField(max_length=255)
+    content = models.TextField()
+    #tags
+    #category
+    counted_views = models.IntegerField(default=0)
+    status = models.BooleanField()
+    published_date = models.DateTimeField()
+    created_date = models.DateTimeField(auto_now_add=True)
+    updated_date = models.DateTimeField(auto_now=True)
