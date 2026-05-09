16cfcaccee059e6ea9e6c91efc9260840d6b080f
Moory <mortezafps646@gmail.com>
2026-05-09 16:59:53 +0330

    update post part

--- blog/views.py
+++ blog/views.py
@@ -11,5 +11,15 @@ def blog_single(request, pk):
     post = get_object_or_404(Post, pk=pk, status=True)
     post.counted_views += 1
     post.save()
-    context = {'post': post}
+
+    all_posts = list(Post.objects.filter(status=True, published_date__lte=timezone.now())) 
+    current_index = all_posts.index(post) 
+    previous_post = all_posts[current_index - 1] if current_index > 0 else None
+    next_post = all_posts[current_index + 1] if current_index < len(all_posts) - 1 else None
+
+    context = {
+        'post': post,
+        'previous_post': previous_post,
+        'next_post': next_post,
+    }
     return render(request, "blog/blog-single.html", context)
