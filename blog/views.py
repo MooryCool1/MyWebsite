fbafbfcebb67e82e16d2d189488f8fe5b9280a4c
Moory <mortezafps646@gmail.com>
2026-05-10 15:59:15 +0330

    Update views

--- blog/views.py
+++ blog/views.py
@@ -8,12 +8,12 @@ def blog_view(request):
     return render(request, "blog/blog-home.html", context)
 
 def blog_single(request, pk):
-    post = get_object_or_404(Post, pk=pk, status=True)
+    post = get_object_or_404(Post, pk=pk, status=True, published_date__lte=timezone.now())
     post.counted_views += 1
     post.save()
 
-    all_posts = list(Post.objects.filter(status=True, published_date__lte=timezone.now())) 
-    current_index = all_posts.index(post) 
+    all_posts = list(Post.objects.filter(status=True, published_date__lte=timezone.now()))
+    current_index = all_posts.index(post)
     previous_post = all_posts[current_index - 1] if current_index > 0 else None
     next_post = all_posts[current_index + 1] if current_index < len(all_posts) - 1 else None
 
