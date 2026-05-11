77e072b364fbd10f9e44df5302c32d8a91a1bd5b
Moory <mortezafps646@gmail.com>
2026-05-11 18:19:52 +0330

    .

--- blog/templatetags/blog_tags.py
+++ blog/templatetags/blog_tags.py
@@ -1,3 +1,19 @@
 from django import template
-
+from django.utils import timezone
+from blog.models import Post
+from blog.models import Category
 register = template.Library()
+
+@register.inclusion_tag('blog/blog-popular-post.html')
+def latestposts(arg=3):
+    posts = Post.objects.filter(status=True, published_date__lte=timezone.now()).order_by('-published_date')[:arg]
+    return {'posts': posts}
+
+@register.inclusion_tag('blog/blog-post-category.html')
+def postcategories():
+    posts = Post.objects.filter(status=True)
+    categories = Category.objects.all()
+    cat_dict = {}
+    for name in categories:
+        cat_dict[name]=posts.filter(category=name).count()
+    return {'categories':cat_dict}
