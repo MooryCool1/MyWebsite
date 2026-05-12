239bf85e4abae96722785de4684f8d98d4d364e4
Moory <mortezafps646@gmail.com>
2026-05-12 15:42:24 +0330

    .

--- blog/urls.py
+++ blog/urls.py
@@ -6,4 +6,5 @@ app_name = 'blog'
 urlpatterns = [
     path('', views.blog_view, name='index'),
     path('<int:pk>/', views.blog_single, name='single'),
+    path('category/<str:cat_name>', views.blog_view, name='category'),
 ]
--- blog/views.py
+++ blog/views.py
@@ -2,8 +2,11 @@ from django.shortcuts import render, get_object_or_404
 from django.utils import timezone
 from blog.models import Post
 
-def blog_view(request):
+def blog_view(request, cat_name=None):
     posts = Post.objects.filter(status=True, published_date__lte=timezone.now())
+    if cat_name:    
+        posts = posts.filter(category__name=cat_name)
+
     context = {'posts': posts}
     return render(request, "blog/blog-home.html", context)
 
@@ -11,15 +14,20 @@ def blog_single(request, pk):
     post = get_object_or_404(Post, pk=pk, status=True, published_date__lte=timezone.now())
     post.counted_views += 1
     post.save()
-
     all_posts = list(Post.objects.filter(status=True, published_date__lte=timezone.now()))
     current_index = all_posts.index(post)
     previous_post = all_posts[current_index - 1] if current_index > 0 else None
     next_post = all_posts[current_index + 1] if current_index < len(all_posts) - 1 else None
-
     context = {
         'post': post,
         'previous_post': previous_post,
         'next_post': next_post,
     }
     return render(request, "blog/blog-single.html", context)
+
+# def blog_category(request, cat_name):
+#     posts = Post.objects.filter(status=True)
+#     posts = posts.filter(category__name=cat_name)
+#     context = {'posts': posts}
+#     return render(request, "blog/blog-home.html", context)
+# 
