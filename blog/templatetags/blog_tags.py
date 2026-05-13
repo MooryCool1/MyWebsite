3927018bf618cf86485cbfde74e647248f123272
Moory <mortezafps646@gmail.com>
2026-05-13 20:10:58 +0330

    .

--- blog/templatetags/blog_tags.py
+++ blog/templatetags/blog_tags.py
@@ -17,3 +17,8 @@ def postcategories():
     for name in categories:
         cat_dict[name]=posts.filter(category=name).count()
     return {'categories':cat_dict}
+
+@register.inclusion_tag('website/latest-posts.html')
+def latestblogposts(arg=6):
+    posts = Post.objects.filter(status=True, published_date__lte=timezone.now()).order_by('-published_date')[:arg]
+    return {'posts': posts}
