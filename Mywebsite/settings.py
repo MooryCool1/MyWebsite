38c919f78dd244f16f1b227ba8e40b0a5d332479
Moory <moorycool12@gmail.com>
2026-06-12 18:18:19 +0330

    UPDATE

--- Mywebsite/settings.py
+++ Mywebsite/settings.py
@@ -162,7 +162,11 @@ INTERNAL_IPS = [
 ]
 
 X_FRAME_OPTIONS = 'SAMEORIGIN'
-
-EMAIL_BACKEND = 'django.core.mail.backends.filebased.EmailBackend'
-EMAIL_FILE_PATH = BASE_DIR / 'sent_emails'
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+EMAIL_HOST_USER = 'mortezafps646@gmail.com'
+EMAIL_HOST_PASSWORD = 'oqkvtnjjhucgopyf'
+DEFAULT_FROM_EMAIL = 'mortezafps646@gmail.com'
 PASSWORD_RESET_TIMEOUT = 86400 
