95257b08311f9a704e878ebc0d5d0c20fe426e57
Moory <moorycool12@gmail.com>
2026-06-12 21:54:29 +0330

    پ

--- Mywebsite/setting/prod.py
+++ Mywebsite/setting/prod.py
@@ -1,19 +1,19 @@
 from Mywebsite.settings import *
-
+import os
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/6.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-!r&%%qk%#v*_lv78khn6ew2#9p%ir0^3!joll(e+9f@oe0vrc%'
 
+SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['php-e4xx6g.chbkn.run']
 
 
 # site framework
-SITE_ID = 3
+SITE_ID = 1
 
 
 # CSRF_COOKIE_SECURE = True
@@ -21,7 +21,7 @@ SITE_ID = 3
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'NAME': '/var/data/db.sqlite3',
     }
 }
 
--- Mywebsite/settings.py
+++ Mywebsite/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/6.0/ref/settings/
 """
 
 from pathlib import Path
-
+import os
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -35,7 +35,6 @@ INSTALLED_APPS = [
     'robots',
     'captcha',
     'django_summernote',
-    'debug_toolbar',
     'taggit',
     'website.apps.WebsiteConfig',
     'blog.apps.BlogConfig', 
@@ -88,7 +87,6 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'debug_toolbar.middleware.DebugToolbarMiddleware',
 ]
 
 ROOT_URLCONF = 'Mywebsite.urls'
@@ -166,7 +164,7 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = 'mortezafps646@gmail.com'
-EMAIL_HOST_PASSWORD = 'oqkvtnjjhucgopyf'
+EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER')
+EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASSWORD')
 DEFAULT_FROM_EMAIL = 'mortezafps646@gmail.com'
 PASSWORD_RESET_TIMEOUT = 86400 
