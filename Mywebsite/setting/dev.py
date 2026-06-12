34ce2dcaf0aa91766b37e5426d6ef06c7d48c5d4
Moory <mortezafps646@gmail.com>
2026-06-12 16:57:36 +0330

    .

---
+++ Mywebsite/setting/dev.py
@@ -0,0 +1,31 @@
+from Mywebsite.settings import *
+# Quick-start development settings - unsuitable for production
+# See https://docs.djangoproject.com/en/6.0/howto/deployment/checklist/
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = 'django-insecure-!r&%%qk%#v*_lv78khn6ew2#9p%ir0^3!joll(e+9f@oe0vrc%'
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+ALLOWED_HOSTS = []
+
+
+# site framework
+SITE_ID = 3
+
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
+}
+
+STATIC_ROOT = BASE_DIR / "staticfiles"  
+STATICFILES_DIRS = [
+    BASE_DIR / 'statics', 
+]
+MEDIA_ROOT = BASE_DIR / 'media'
+
+X_FRAME_OPTIONS = 'SAMEORIGIN'
--- Mywebsite/settings.py
+++ Mywebsite/settings.py
@@ -16,16 +16,6 @@ from pathlib import Path
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 
-# Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/6.0/howto/deployment/checklist/
-
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-!r&%%qk%#v*_lv78khn6ew2#9p%ir0^3!joll(e+9f@oe0vrc%'
-
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
-
-ALLOWED_HOSTS = []
 
 
 # Application definition
@@ -52,8 +42,6 @@ INSTALLED_APPS = [
     'accounts'
 ]
 
-# site framework
-SITE_ID = 3
 
 # robots
 ROBOTS_USE_HOST = True
@@ -125,12 +113,7 @@ WSGI_APPLICATION = 'Mywebsite.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/6.0/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
+
 
 
 # Password validation
@@ -167,16 +150,12 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
-STATIC_ROOT = BASE_DIR / "staticfiles"  # ← فقط این رو عوض کن
+
 
 STATIC_URL = '/static/'
 
-STATICFILES_DIRS = [
-    BASE_DIR / 'statics', 
-]
-
 MEDIA_URL = '/media/'
-MEDIA_ROOT = BASE_DIR / 'media'
+
 
 INTERNAL_IPS = [
     '127.0.0.1', 
