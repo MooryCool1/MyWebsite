62e961f9afeffbc63ca92bbb49ee7ba47d788054
Moory <moorycool12@gmail.com>
2026-06-12 21:06:25 +0330

    ./

--- Mywebsite/wsgi.py
+++ Mywebsite/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Mywebsite.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Mywebsite.setting.prod')
 
 application = get_wsgi_application()
